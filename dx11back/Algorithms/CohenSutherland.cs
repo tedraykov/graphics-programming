ccdac92413ea4b0b01e8a83e67f93d40c175e7bf
Teodor Raykov <tedraykov@gmail.com>
2020-05-23 22:19:17 +0300

    dx11 source no longer a submodule, but part of main repo

---
+++ dx11back/Algorithms/CohenSutherland.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms {
+    /// <summary>
+    /// http://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
+    /// </summary>
+    public static class CohenSutherland {
+
+        [Flags]
+        enum OutCode {
+            Inside = 0,
+            Left = 1,
+            Right = 2,
+            Bottom = 4,
+            Top = 8
+        }
+
+        private static OutCode ComputeOutCode(float x, float y, RectangleF r) {
+            var code = OutCode.Inside;
+
+            if (x < r.Left) code |= OutCode.Left;
+            if (x > r.Right) code |= OutCode.Right;
+            if (y < r.Top) code |= OutCode.Top;
+            if (y > r.Bottom) code |= OutCode.Bottom;
+
+            return code;
+        }
+        private static OutCode ComputeOutCode(PointF p, RectangleF r) { return ComputeOutCode(p.X, p.Y, r); }
+
+        public static Tuple<PointF, PointF> ClipSegment(RectangleF r, PointF p1, PointF p2) {
+            // classify the endpoints of the line
+            var outCodeP1 = ComputeOutCode(p1, r);
+            var outCodeP2 = ComputeOutCode(p2, r);
+            var accept = false;
+
+            while (true) { // should only iterate twice, at most
+                // Case 1:
+                // both endpoints are within the clipping region
+                if ((outCodeP1 | outCodeP2) == OutCode.Inside) {
+                    accept = true;
+                    break;
+                }
+
+                // Case 2:
+                // both endpoints share an excluded region, impossible for a line between them to be within the clipping region
+                if ((outCodeP1 & outCodeP2) != 0) {
+                    break;
+                }
+
+                // Case 3:
+                // The endpoints are in different regions, and the segment is partially within the clipping rectangle
+
+                // Select one of the endpoints outside the clipping rectangle
+                var outCode = outCodeP1 != OutCode.Inside ? outCodeP1 : outCodeP2;
+
+                // calculate the intersection of the line with the clipping rectangle using parametric line equations
+                var p = CalculateIntersection(r, p1, p2, outCode);
+
+                // update the point after clipping and recaluculate outcode
+                if (outCode == outCodeP1) {
+                    p1 = p;
+                    outCodeP1 = ComputeOutCode(p1, r);
+                } else {
+                    p2 = p;
+                    outCodeP2 = ComputeOutCode(p2, r);
+                }
+            }
+            // if clipping area contained a portion of the line
+            if (accept) {
+                return new Tuple<PointF, PointF>(p1, p2);
+            }
+
+            // the line did not intersect the clipping area
+            return null;
+        }
+
+        private static PointF CalculateIntersection(RectangleF r, PointF p1, PointF p2, OutCode clipTo) {
+            var dx = (p2.X - p1.X);
+            var dy = (p2.Y - p1.Y);
+
+            var slopeY = dx / dy; // slope to use for possibly-vertical lines
+            var slopeX = dy / dx; // slope to use for possibly-horizontal lines
+
+            if (clipTo.HasFlag(OutCode.Top)) {
+                return new PointF(
+                    p1.X + slopeY * (r.Top - p1.Y),
+                    r.Top
+                    );
+            }
+            if (clipTo.HasFlag(OutCode.Bottom)) {
+                return new PointF(
+                    p1.X + slopeY * (r.Bottom - p1.Y),
+                    r.Bottom
+                    );
+            }
+            if (clipTo.HasFlag(OutCode.Right)) {
+                return new PointF(
+                    r.Right,
+                    p1.Y + slopeX * (r.Right - p1.X)
+                    );
+            }
+            if (clipTo.HasFlag(OutCode.Left)) {
+                return new PointF(
+                    r.Left,
+                    p1.Y + slopeX * (r.Left - p1.X)
+                    );
+            }
+            throw new ArgumentOutOfRangeException("clipTo = " + clipTo);
+        }
+    }
+}
---
+++ dx11back/BoxDemo/Program.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Core;
+using SlimDX;
+using SlimDX.D3DCompiler;
+using SlimDX.DXGI;
+using SlimDX.Direct3D11;
+using Buffer = SlimDX.Direct3D11.Buffer;
+using Debug = System.Diagnostics.Debug;
+
+namespace BoxDemo {
+    using Core.Vertex;
+
+    using Effect = SlimDX.Direct3D11.Effect;
+
+    public class BoxApp : D3DApp {
+    private Buffer _boxVB;
+    private Buffer _boxIB;
+
+    private Effect _fx;
+    private EffectTechnique _tech;
+    private EffectMatrixVariable _fxWVP;
+
+    private InputLayout _inputLayout;
+
+    // Matrices
+    private Matrix _world;
+    private Matrix _view;
+    private Matrix _proj;
+
+    // Camera variables
+    private float _theta;
+    private float _phi;
+    private float _radius;
+
+    private Point _lastMousePos;
+
+    private bool _disposed;
+
+    public BoxApp(IntPtr hInstance) : base(hInstance) {
+        _boxIB = null;
+        _boxVB = null;
+        _fx = null;
+        _tech = null;
+        _fxWVP = null;
+        _inputLayout = null;
+        _theta = 1.5f * MathF.PI;
+        _phi = 0.25f * MathF.PI;
+        _radius = 5.0f;
+
+        MainWindowCaption = "Box Demo";
+        _lastMousePos = new Point(0, 0);
+        _world = Matrix.Identity;
+        _view = Matrix.Identity;
+        _proj = Matrix.Identity;
+    }
+    protected override void Dispose(bool disposing) {
+        if (!_disposed) {
+            if (disposing) {
+                Util.ReleaseCom(ref _boxVB);
+                Util.ReleaseCom(ref _boxIB);
+                Util.ReleaseCom(ref _fx);
+                Util.ReleaseCom(ref _inputLayout);
+            }
+            _disposed = true;
+        }
+        base.Dispose(disposing);
+    }
+
+    public override bool Init() {
+        if (!base.Init()) {
+            return false;
+        }
+        BuildGeometryBuffers();
+        BuildFX();
+        BuildVertexLayout();
+
+        return true;
+    }
+    public override void OnResize() {
+        base.OnResize();
+        // Recalculate perspective matrix
+        _proj = Matrix.PerspectiveFovLH(0.25f * MathF.PI, AspectRatio, 1.0f, 1000.0f);
+    }
+    public override void UpdateScene(float dt) {
+        base.UpdateScene(dt);
+
+        // Get camera position from polar coords
+        var x = _radius * MathF.Sin(_phi) * MathF.Cos(_theta);
+        var z = _radius * MathF.Sin(_phi) * MathF.Sin(_theta);
+        var y = _radius * MathF.Cos(_phi);
+
+        // Build the view matrix
+        var pos = new Vector3(x, y, z);
+        var target = new Vector3(0);
+        var up = new Vector3(0, 1, 0);
+        _view = Matrix.LookAtLH(pos, target, up);
+
+    }
+    public override void DrawScene() {
+        base.DrawScene();
+        ImmediateContext.ClearRenderTargetView(RenderTargetView, Color.LightSteelBlue);
+        ImmediateContext.ClearDepthStencilView(DepthStencilView, DepthStencilClearFlags.Depth | DepthStencilClearFlags.Stencil, 1.0f, 0);
+
+        ImmediateContext.InputAssembler.InputLayout = _inputLayout;
+        ImmediateContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+
+        ImmediateContext.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(_boxVB, VertexPC.Stride, 0));
+        ImmediateContext.InputAssembler.SetIndexBuffer(_boxIB, Format.R32_UInt, 0);
+
+        var wvp = _world * _view * _proj;
+        _fxWVP.SetMatrix(wvp);
+
+        for (int p = 0; p < _tech.Description.PassCount; p++) {
+            _tech.GetPassByIndex(p).Apply(ImmediateContext);
+            ImmediateContext.DrawIndexed(36, 0, 0);
+        }
+        SwapChain.Present(0, PresentFlags.None);
+
+    }
+
+    protected override void OnMouseDown(object sender, MouseEventArgs mouseEventArgs) {
+        _lastMousePos = mouseEventArgs.Location;
+        Window.Capture = true;
+    }
+    protected override void OnMouseUp(object sender, MouseEventArgs e) {
+        Window.Capture = false;
+    }
+    protected override void OnMouseMove(object sender, MouseEventArgs e) {
+        if (e.Button == MouseButtons.Left) {
+            var dx = MathF.ToRadians(0.25f * (e.X - _lastMousePos.X));
+            var dy = MathF.ToRadians(0.25f * (e.Y - _lastMousePos.Y));
+
+            _theta += dx;
+            _phi += dy;
+
+            _phi = MathF.Clamp(_phi, 0.1f, MathF.PI - 0.1f);
+        } else if (e.Button == MouseButtons.Right) {
+            var dx = 0.005f * (e.X - _lastMousePos.X);
+            var dy = 0.005f * (e.Y - _lastMousePos.Y);
+            _radius += dx - dy;
+
+            _radius = MathF.Clamp(_radius, 3.0f, 15.0f);
+        }
+        _lastMousePos = e.Location;
+    }
+
+    private void BuildGeometryBuffers() {
+        var vertices = new[] {
+            new VertexPC(new Vector3(-1.0f, -1.0f, -1.0f), Color.White),
+            new VertexPC(new Vector3(-1, 1, -1), Color.Black),
+            new VertexPC(new Vector3(1,1,-1), Color.Red ),
+            new VertexPC( new Vector3(1,-1,-1), Color.Green ),
+            new VertexPC(new Vector3(-1,-1,1),Color.Blue ),
+            new VertexPC(new Vector3(-1,1,1), Color.Yellow ),
+            new VertexPC(new Vector3(1,1,1), Color.Cyan ),
+            new VertexPC(new Vector3(1,-1,1),Color.Magenta )
+        };
+        var vbd = new BufferDescription(
+            VertexPC.Stride*vertices.Length, 
+            ResourceUsage.Immutable, 
+            BindFlags.VertexBuffer, 
+            CpuAccessFlags.None, 
+            ResourceOptionFlags.None, 
+            0);
+        _boxVB = new Buffer(Device, new DataStream(vertices, true, false), vbd);
+
+        var indices = new uint[] {
+            // front
+            0,1,2,
+            0,2,3,
+            // back
+            4,6,5,
+            4,7,6,
+            // left
+            4,5,1,
+            4,1,0,
+            // right
+            3,2,6,
+            3,6,7,
+            //top
+            1,5,6,
+            1,6,2,
+            // bottom
+            4,0,3,
+            4,3,7
+        };
+        var ibd = new BufferDescription(
+            sizeof (uint)*indices.Length, 
+            ResourceUsage.Immutable, 
+            BindFlags.IndexBuffer, 
+            CpuAccessFlags.None, 
+            ResourceOptionFlags.None, 
+            0);
+        _boxIB = new Buffer(Device, new DataStream(indices, false, false), ibd);
+    }
+    private void BuildFX() {
+        var shaderFlags = ShaderFlags.None;
+        #if DEBUG
+            shaderFlags |= ShaderFlags.Debug;
+            shaderFlags |= ShaderFlags.SkipOptimization;
+        #endif
+        string errors = null;
+        ShaderBytecode compiledShader = null;
+        try {
+            compiledShader = ShaderBytecode.CompileFromFile(
+                "FX/color.fx", 
+                null, 
+                "fx_5_0", 
+                shaderFlags, 
+                EffectFlags.None, 
+                null, 
+                null, 
+                out errors);
+            _fx = new Effect(Device, compiledShader);
+        } catch (Exception ex) {
+            if (!string.IsNullOrEmpty(errors)) {
+                MessageBox.Show(errors);
+            }
+            MessageBox.Show(ex.Message);
+            return;
+        } finally {
+            Util.ReleaseCom(ref compiledShader);
+        }
+
+        _tech = _fx.GetTechniqueByName("ColorTech");
+        _fxWVP = _fx.GetVariableByName("gWorldViewProj").AsMatrix();
+    }
+    private void BuildVertexLayout() {
+        var vertexDesc = new[] {
+            new InputElement("POSITION", 0, Format.R32G32B32_Float, 
+                0, 0, InputClassification.PerVertexData, 0),
+            new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 
+                12, 0, InputClassification.PerVertexData, 0)
+        };
+        Debug.Assert(_tech != null);
+        var passDesc = _tech.GetPassByIndex(0).Description;
+        _inputLayout = new InputLayout(Device, passDesc.Signature, vertexDesc);
+    }
+}
+
+
+    static class Program {
+        static void Main(string[] args) {
+            Configuration.EnableObjectTracking = true;
+            var app = new BoxApp(Process.GetCurrentProcess().Handle);
+            if (!app.Init()) {
+                return;
+            }
+            app.Run();
+        }
+    }
+}
